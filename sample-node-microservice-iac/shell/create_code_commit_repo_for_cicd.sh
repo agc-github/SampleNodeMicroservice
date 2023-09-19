80cbeaf8236be31074c2484cc152d63f41bf11b8
Ajay Chauhan <ajayc.inbox@gmail.com>
2023-09-19 22:50:31 +0530

    code optimisation for code clarity

--- sample-node-microservice-iac/shell/create_code_commit_repo_for_cicd.sh
+++ sample-node-microservice-iac/shell/create_code_commit_repo_for_cicd.sh
@@ -1,12 +1,20 @@
-rm -r -f sample-node-microservice-cicd-repository
+rm -r -f temp
 
 docker run --rm -ti -v ~/.aws:/root/.aws -v ./:/aws amazon/aws-cli codecommit create-repository --repository-name sample-node-microservice-cicd-repository --repository-description "repo to store taskdef.json and appspec.yaml files for sample-node-microservice CI/CD setup" > /dev/null
+
+mkdir temp
+
+cd temp
 
 git clone https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/sample-node-microservice-cicd-repository > /dev/null
 
 cd sample-node-microservice-cicd-repository
 
-cp ../sample-node-microservice-cicd/*.* .
+git config --local user.name "AWS CLI"
+
+git config --local user.email "AWS CLI"
+
+cp ../../sample-node-microservice-cicd/*.* .
 
 git add .
 
@@ -14,6 +22,6 @@ git commit -m "for sample-node-microservice CI/CD setup"
 
 git push
 
-cd ..
+cd ../..
 
-rm -r -f sample-node-microservice-cicd-repository
+rm -r -f temp
