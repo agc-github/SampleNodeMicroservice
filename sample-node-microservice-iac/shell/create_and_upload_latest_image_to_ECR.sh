12d2a508f1f57b32e7ac6b9faa994fda9be9f82e
Ajay Chauhan <ajayc.inbox@gmail.com>
2023-09-20 23:10:51 +0530

    updated with new AWS Account ID value
    updated with new AWS Account ID value

--- sample-node-microservice-iac/shell/create_and_upload_latest_image_to_ECR.sh
+++ sample-node-microservice-iac/shell/create_and_upload_latest_image_to_ECR.sh
@@ -1,5 +1,5 @@
 cd sample-node-microservice-impl
 rm -r node_modules
-docker build . -t 488314167089.dkr.ecr.ap-south-1.amazonaws.com/sample-node-microservice/service:latest
-docker run --rm -ti -v ~/.aws:/root/.aws -v ./:/aws amazon/aws-cli ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 488314167089.dkr.ecr.ap-south-1.amazonaws.com
-docker push 488314167089.dkr.ecr.ap-south-1.amazonaws.com/sample-node-microservice/service:latest
+docker build . -t 147629156133.dkr.ecr.ap-south-1.amazonaws.com/sample-node-microservice/service:latest
+docker run --rm -ti -v ~/.aws:/root/.aws -v ./:/aws amazon/aws-cli ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 147629156133.dkr.ecr.ap-south-1.amazonaws.com
+docker push 147629156133.dkr.ecr.ap-south-1.amazonaws.com/sample-node-microservice/service:latest
